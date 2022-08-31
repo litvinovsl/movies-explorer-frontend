0ed2677e647835b19a1c0550329629337d502fe3
Sergey Litvinov <litvinows9@gmail.com>
2022-08-31 12:52:31 +0300

    created and used component Techs

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,7 +3,7 @@ import './Main.css';
 import Promo from '../Promo/Promo';
 import NavTab from '../NavTab/NavTab';
 import AboutProject from '../AboutProject/AboutProject';
-// import Techs from '../Techs/Techs';
+import Techs from '../Techs/Techs';
 // import AboutMe from '../AboutMe/AboutMe';
 
 function Main() {
@@ -12,7 +12,7 @@ function Main() {
             <Promo />
             <NavTab />
             <AboutProject />
-            {/* <Techs /> */}
+            <Techs />
             {/* <AboutMe /> */}
         </main>
     );
