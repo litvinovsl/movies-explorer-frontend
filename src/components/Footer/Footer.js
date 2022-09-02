5527132b0dd8dc26b7df80d0083f9827fdd009d4
Sergey Litvinov <litvinows9@gmail.com>
2022-09-02 23:46:26 +0300

    add github and made pissing

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -11,7 +11,10 @@ function Footer() {
             <p className='footer__text footer__educational-project'>Учебный проект Яндекс.Практикум х BeatFilm.</p>
             <div className='footer__copyright'>
                 <p className='footer__text footer__copyright-text'>© {year}</p>
-                <p className='footer__text footer__copyright-text'>Яндекс.Практикум</p>
+                <div className='footer__links'>
+                    <a href='https://practicum.yandex.ru/' className='footer__text footer__copyright-text footer__copyright-text_hover-opacity'>Яндекс.Практикум</a>
+                    <a href='https://github.com/' className='footer__text footer__copyright-text footer__copyright-text_hover-opacity'>Github</a>
+                </div>
             </div>
         </section>
     );
