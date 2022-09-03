c1498195ef871f4a28c8cb923de67f84b70bcb86
Sergey Litvinov <litvinows9@gmail.com>
2022-09-03 17:11:22 +0300

    added preloader component

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Movies.css';
 import SearchForm from '../SearchForm/SearchForm';
+import Preloader from '../Preloader/Preloader';
 
 function Movies() {
     return (
         <main className='movies'>
             <SearchForm />
+            <Preloader />
         </main>
     );
 }
