486a1c8c5c878d04a80507209a5840bbac557006
Sergey Litvinov <litvinows9@gmail.com>
2022-09-04 18:24:43 +0300

    working with the burger menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,15 +3,28 @@ import { Link } from 'react-router-dom';
 import './Header.css';
 import iconProfile from '../../images/icon_profile.svg';
 import logo from '../../images/logo.svg';
+import MenuPopup from '../MenuPopup/MenuPopup'
 
 
 
 function Header() {
+  const[isOpenMenu, setIsOpenMenu] = React.useState(false);
+  function handleClickBurgerMenu(){
+    setIsOpenMenu(true);
+  }
+  function closeMenu(){
+    setIsOpenMenu(false);
+  }
+
   return (
     <section className="header">
+      <MenuPopup 
+        isOpen={isOpenMenu}
+        onClose={closeMenu}/>
       <Link to="/" >
         <img className='header__logo' src={logo} alt='логотип сайта' />
       </Link>
+      <button className='header__burger' onClick={handleClickBurgerMenu} />
       <div className='header__links-and-button'>
         <nav className='header__links'>
           <Link to="/movies" className='header__link'>Фильмы</Link>
---
+++ src/components/MenuPopup/MenuPopup.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './MenuPopup.css';
+import iconProfile from '../../images/icon_profile.svg';
+import logo from '../../images/logo.svg';
+
+
+
+function MenuPopup(props) {
+
+  
+  return (
+    <section className={`popup ${props.isOpen && "popup_opened"}`}>
+      <div className="popup__container">
+        <div>
+        <button className='popup__close' onClick={props.onClose}/>
+        <nav className='popup__links'>
+            <Link to="/" onClick={props.onClose} className='popup__link'>Главная страница</Link>
+            <Link to="/movies" onClick={props.onClose} className='popup__link'>Фильмы</Link>
+            <Link to="/saved-movies" onClick={props.onClose} className='popup__link'>Сохраненные фильмы</Link>
+        </nav>
+        </div>
+        <div>
+        <Link to="/profile" className='popup__profile'>
+          <p className='popup__profile-text'>Аккаунт</p>
+          <div className='popup__profile-icon-border'>
+            <img className='popup__profile-icon' src={iconProfile} alt='иконка профиля' />
+          </div>
+        </Link>
+        </div>
+      </div>
+      <div id="overlayProfile" onClick={props.onClose} className="popup__overlay"></div>
+    </section>
+  );
+}
+
+export default MenuPopup;
