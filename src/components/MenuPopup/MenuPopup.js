d396b57ac99ad2d5538b43be393ed8f050f7a36f
Sergey Litvinov <litvinows9@gmail.com>
2022-09-11 12:57:56 +0300

    add signin and signup link

--- src/components/MenuPopup/MenuPopup.js
+++ src/components/MenuPopup/MenuPopup.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './MenuPopup.css';
+import { Route } from "react-router-dom";
+
 import MenuLink from '../MenuLink/MenuLink';
 import ProfileButton from '../ProfileButton/ProfileButton';
 
@@ -9,21 +11,35 @@ function MenuPopup(props) {
     <section className={`popup ${props.isOpen && "popup_opened"}`}>
       <div className="popup__container">
         <button className='popup__close' onClick={props.onClose} />
-        <nav className='popup__links'>
-          <MenuLink
-            text='Главная страница'
-            route='/' 
-            class='nav-link_burger'/>
-          <MenuLink
-            text='Фильмы'
-            route='/movies' 
-            class='nav-link_burger'/>
-          <MenuLink
-            text='Сохраненные фильмы'
-            route='/saved-movies' 
-            class='nav-link_burger'/>
-        </nav>
-        <ProfileButton />
+        <Route exact path='/'>
+          <nav className='popup__links'>
+            <MenuLink
+              text='Регистрация'
+              route='/signup'
+              class='nav-link_burger' />
+            <MenuLink
+              text='Войти'
+              route='/signin'
+              class='nav-link_burger' />
+          </nav>
+        </Route>
+        <Route exact path={['/movies', '/saved-movies', '/profile']}>
+          <nav className='popup__links'>
+            <MenuLink
+              text='Главная страница'
+              route='/'
+              class='nav-link_burger' />
+            <MenuLink
+              text='Фильмы'
+              route='/movies'
+              class='nav-link_burger' />
+            <MenuLink
+              text='Сохраненные фильмы'
+              route='/saved-movies'
+              class='nav-link_burger' />
+          </nav>
+          <ProfileButton />
+        </Route>
       </div>
       <div id="overlayProfile" onClick={props.onClose} className="popup__overlay"></div>
     </section>
