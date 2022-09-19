36b11b13f9ae58ba319ea072b688cb12fda267bb
Sergey Litvinov <litvinows9@gmail.com>
2022-09-19 21:22:10 +0300

    only for /movies page

--- src/components/MoreMovies/MoreMovies.js
+++ src/components/MoreMovies/MoreMovies.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import './MoreMovies.css';
 
 function MoreMovies() {
     return (
-        <div className='more-movies'>
-        <button className='more-movies__button'>Ещё</button>
-        </div>
+        <Route exact path='/movies'>
+            <div className='more-movies'>
+                <button className='more-movies__button'>Ещё</button>
+            </div>
+        </Route>
     );
 }
 
