802f43f8f20776f66ea110bd3544c734df10e252
Sergey Litvinov <litvinows9@gmail.com>
2022-09-22 21:56:20 +0300

    displaying the number of cards according to the screen width

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,14 +1,86 @@
-import React from 'react'
+import { useState, useEffect, useContext, useCallback } from 'react';
 import { Route, Switch } from "react-router-dom";
 import './MoviesCardList.css'
 import MoviesCard from '../MoviesCard/MoviesCard';
 import { CurrentUserContext } from '../../context/CurrentUserContext';
 
 function MoviesCardList() {
-    const context = React.useContext(CurrentUserContext);
+    const context = useContext(CurrentUserContext);
     const { moviesWithLikeState, savedMovies, savedMoviesWithFilter } = context;
-    // console.log('asdadadad', savedMovies)
+    const [renderedMovieQuantity, setRenderedMovieQuantity] = useState(null);
+    const [renderedMovieCards, setRenderedMovieCards] = useState([]);
+    const getWidth = () => window.innerWidth
+        || document.documentElement.clientWidth
+        || document.body.clientWidth;
+    const widthSize = useCurrentWidth();
+    // console.log(widthSize);
+
+    // const cardDisplay = useCallback(() = {
     
+    // }, []);
+    const cardDisplay = useCallback(() =>{
+        if( widthSize > 1279){
+            setRenderedMovieQuantity(12);
+            // console.log('setRenderedMovieQuantity(12);', renderedMovieQuantity)
+        }
+        else if( widthSize > 783){
+            setRenderedMovieQuantity(8);
+            // console.log('setRenderedMovieQuantity(8);',renderedMovieQuantity)
+        }
+        else if( widthSize <= 783){
+            setRenderedMovieQuantity(5);
+            // console.log('setRenderedMovieQuantity(5);',renderedMovieQuantity)
+        }
+    },[widthSize])
+
+    useEffect(() => {
+		cardDisplay();
+	}, [cardDisplay, widthSize]);
+
+    useEffect(() => {
+		setRenderedMovieCards(moviesWithLikeState.slice(0, renderedMovieQuantity));
+        // console.log('rendercards: ', renderedMovieCards);
+        // console.log('allmovies: ', moviesWithLikeState);
+	}, [moviesWithLikeState, renderedMovieQuantity, setRenderedMovieCards]);
+
+
+    
+
+    function useCurrentWidth() {
+        // save current window width in the state object
+        let [width, setWidth] = useState(getWidth());
+
+        // in this case useEffect will execute only once because
+        // it does not have any dependencies.
+        useEffect(() => {
+            // timeoutId for debounce mechanism
+            let timeoutId = null;
+            const resizeListener = () => {
+                // prevent execution of previous setTimeout
+                clearTimeout(timeoutId);
+                // change width from the state object after 150 milliseconds
+                timeoutId = setTimeout(() => setWidth(getWidth()), 600);
+            };
+            // set resize listener
+            window.addEventListener('resize', resizeListener);
+
+            // clean up function
+            return () => {
+                // remove resize listener
+                window.removeEventListener('resize', resizeListener);
+            }
+        }, [])
+
+        return width;
+    }
+    
+
+
+
+    // console.log('asdadadad', savedMovies)
+    // const [handleScreenResize, setHandleScreenResize] = React.useState();
+    //  window.addEventListener('resize', handleScreenResize);
+    // console.log(handleScreenResize);
 
 
 
@@ -17,7 +89,7 @@ function MoviesCardList() {
         <section className="elements">
             <Switch>
                 <Route path="/movies">
-                    {moviesWithLikeState.map((movie) => {
+                    {renderedMovieCards.map((movie) => {
                         if (typeof movie.duration === 'number') {
                             movie.duration = Math.trunc(movie.duration / 60) + 'ч ' + (movie.duration % 60) + 'м';
                         }
